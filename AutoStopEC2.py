b8bd2b52706d57e3c5e7de669b2ff224d19449d7
AWSBeginnerTitorial <awsbeginnertutorial@gmail.com>
2021-09-01 23:13:20 +0700

    update code

---
+++ AutoStopEC2.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+region = 'us-east-1'
+
+def lambda_handler(event, lambda_context):
+    ec2_client = boto3.client("ec2", region_name=region)
+    reservations = ec2_client.describe_instances(Filters=[
+        {
+            "Name": 'tag:Stop',
+            "Values": ['yes'],
+        }
+    ]).get("Reservations")
+
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            ec2_client.stop_instances(InstanceIds=[instance_id])
+            print(f"Instances Stopped " + str(instance_id))
